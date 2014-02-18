1eb5305c98f89a31281b6f4b953364af36511a00
sopier <sopier@gmail.com>
2014-02-18 15:22:16 +0700

    adding > korban & relawan worker to gearman

--- ganapatih_worker.py
+++ ganapatih_worker.py
@@ -9,16 +9,31 @@ db = c["ganapatih"]
 
 gm_worker = gearman.GearmanWorker(["127.0.0.1"])
 
-def insert_dbase(gearman_worker, gearman_job):
-    sys.stdout.write('inserting dbase at '
-                     + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
-                     + '\n')
-    db.panics.insert(json.loads(gearman_job.data))
-    sys.stdout.write('inserting success at '
-                     + datetime.strftime(datetime.now(), "%Y-%m-%d %H:%M:%S")
-                     + '\n')
+def register_insert(gearman_worker, gearman_job):
+    t = datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S")
+    sys.stdout.write('[%s] :: Registering new user\n' % t)
+    db.register.insert(json.loads(gearman_job.data))
+    sys.stdout.write('[%s] :: User registered\n' % t)
     return json.dumps(gearman_job.data)
 
-gm_worker.set_client_id('insert_dbase_worker')
-gm_worker.register_task('register', insert_dbase)
+def korban_insert(gearman_worker, gearman_job):
+    t = datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S")
+    sys.stdout.write('[%s] :: Inserting korban data\n' % t)
+    db.korban.insert(json.loads(gearman_job.data))
+    sys.stdout.write('[%s] :: Korban data inserted\n' % t)
+    return json.dumps(gearman_job.data)
+
+def relawan_insert(gearman_worker, gearman_job):
+    t = datetime.strftime(datetime.now(), "%Y/%m/%d %H:%M:%S")
+    sys.stdout.write('[%s] :: Inserting relawan data\n' % t)
+    db.relawan.insert(json.loads(gearman_job.data))
+    sys.stdout.write('[%s] :: Relawan data inserted\n' % t)
+    return json.dumps(gearman_job.data)
+
+gm_worker.set_client_id('register_new_user')
+gm_worker.register_task('register', register_insert)
+gm_worker.set_client_id('insert_korban')
+gm_worker.register_task('korban', korban_insert)
+gm_worker.set_client_id('insert_relawan')
+gm_worker.register_task('relawan', relawan_insert)
 gm_worker.work()
